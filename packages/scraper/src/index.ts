78191ac0da7cecb14b8ddfb4793e16236624e250
cyacedev <github@cyace.dev>
2022-06-18 20:33:35 +0200

    wip code

--- packages/scraper/src/index.ts
+++ packages/scraper/src/index.ts
@@ -1,1 +1,16 @@
-// Do le scraping
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const url = 'https://www.fedlex.admin.ch/filestore/fedlex.data.admin.ch/eli/cc/24/233_245_233/20220101/de/html/fedlex-data-admin-ch-eli-cc-24-233_245_233-20220101-de-html-6.html'; // URL we're scraping
+const AxiosInstance = axios.create();
+
+AxiosInstance.get(url)
+  .then(
+    response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      // $()
+      console.log(html);
+    }
+  )
+  .catch(console.error);
